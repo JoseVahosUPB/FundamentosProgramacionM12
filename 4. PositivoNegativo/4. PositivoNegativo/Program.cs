a4222225ffef6b8b40cbfde7fc24fea1352b9a42
Jose Vahos UPB <jose.vahosn@upb.edu.co>
2026-02-23 19:14:06 -0500

    El número es positivo o negativo
    Tarea:
    4. Escribir un algoritmo que le permita a un usuario ingresar un número entero, y le diga si el número es positivo o negativo.

---
+++ 4. PositivoNegativo/4. PositivoNegativo/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4.PositivoNegativo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero = 0;
+            Console.WriteLine("Ingrese un número");
+            numero = int.Parse(Console.ReadLine());
+            if (numero>=0)
+            {
+                Console.WriteLine("El número es positivo");
+            }
+            else
+            {
+                Console.WriteLine("El número es negativo");
+            }
+        }
+    }
+}

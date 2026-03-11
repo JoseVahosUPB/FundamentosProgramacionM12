1262e1c56584c69da7e653c1268f9eec62f578c9
Jose Vahos UPB <jose.vahosn@upb.edu.co>
2026-03-11 13:32:07 -0500

    9. Ciclo While

---
+++ 9.CicloWhile/9.CicloWhile/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _9.CicloWhile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*int contador = 1;
+            int acumulador = 0;
+
+            while (contador <= 5)
+            {
+                acumulador += contador;
+                Console.WriteLine($"Contador: {contador} - Acumulador: {acumulador}");
+                contador++;
+            }
+
+            Console.WriteLine($"La suma de los cinco primero números enteros es: {acumulador}");*/
+
+            // Crear un algoritmo que permita calcular la factorial de un número dado por el usuario. (5! = 1*2*3*4*5)
+
+            int contador = 1;
+            int acumulador = 1;
+            int numero = 0;
+
+            Console.WriteLine("Ingrese un número: ");
+            numero = int.Parse(Console.ReadLine());
+
+            while (contador <= numero)
+            {
+                Console.WriteLine($"Contador: {contador} - Acumulador: {acumulador}");
+                acumulador *= contador;
+                contador++;
+            }
+
+            Console.WriteLine($"el factorial de {numero}: {acumulador}");
+        }
+    }
+}

f1958742a4920cb1da7ff3cefff3096749b4d86b
Jose Vahos UPB <jose.vahosn@upb.edu.co>
2026-04-08 12:45:03 -0500

    Parcial 2
    Se requiere diseñar un programa para procesar las notas definitivas de Estadística de un grupo de 12 estudiantes. Las calificaciones están en un rango de 0.0 a 5.0.
    El programa debe realizar las siguientes tareas:
    Ingresar por teclado la nota final de cada estudiante.
    Calcular el promedio general del grupo.
    Clasificar a cada estudiante según su calificación en una de las siguientes categorías:
    Excelente: nota ≥ 4.8
    Bueno: 4.0 ≤ nota ≤ 4.7
    Regular: 3.0 ≤ nota ≤ 3.9
    Descuidado: nota ≤ 2.9
    Contar y mostrar por pantalla el número total de estudiantes en cada categoría.
    Mostrar en pantalla el promedio del grupo y la cantidad de alumnos clasificados en cada nivel.

---
+++ Parcial2/Parcial2/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
+
+namespace Parcial2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float nota = 0f;
+
+            float sumatoria = 0f;
+            float promedio = 0f;
+
+            int excelentes = 0;
+            int buenos = 0;
+            int regulares = 0;
+            int descuidados = 0;
+
+            for (int i = 1; i <= 12; i++)
+            {
+                Console.WriteLine("Ingrese la nota final del estudiante "+i);
+                nota = float.Parse(Console.ReadLine());
+
+                sumatoria += nota;
+
+                if (nota >= 4.8f)
+                {
+                    excelentes++;
+                }
+                else if (nota >= 4.0f && nota < 4.8f)
+                {
+                    buenos++;
+                }
+                else if (nota >= 3.0f && nota < 4.0f)
+                {
+                    regulares++;
+                }
+                else if (nota < 3.0f)
+                {
+                    descuidados++;
+                }
+            }
+
+            promedio = sumatoria / 12f;
+
+            Console.WriteLine("Esta es la cantidad de estudiantes por categoría:");
+            Console.WriteLine("Excelentes: " + excelentes);
+            Console.WriteLine("buenos: " + buenos);
+            Console.WriteLine("regulares: " + regulares);
+            Console.WriteLine("descuidados: " + descuidados);
+            Console.WriteLine();
+            Console.WriteLine("El promedio del grupo es: " + promedio);
+
+        }
+    }
+}

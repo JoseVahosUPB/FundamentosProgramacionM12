d17595806325bf179a512ea0417bde473c632b80
Jose Vahos UPB <jose.vahosn@upb.edu.co>
2026-04-24 13:13:36 -0500

    Corrección 20. Programación modular
    Por alguna razón no se ve el Program.cs en el repositorio

---
+++ 20.ProgramacionModular/20.ProgramacionModular/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace _20.ProgramacionModular
+{
+    internal class Program
+    {
+        static int añoActual = 2026;
+
+        static void Main(string[] args)
+        {
+            //Programación modular
+            string nombre = "Ferney";
+            string apellidos = "Chica Álvarez";
+            Console.WriteLine($"Edad: {EdadAñoNacimiento()}");
+            Saludo(nombre, apellidos);
+            Saludo("Carlos", "Peréz Lopez");
+            Console.WriteLine($"Edad: {EdadAñoNacimiento(2001)}");
+            Console.ReadKey();
+            BorrarPantalla();
+        }
+
+        //Procedimientos sin parámetros
+        static void BorrarPantalla()
+        {
+            Console.Clear();
+        }
+
+        //Procedimientos con parámetros
+
+        static void Saludo(string nombre, string apellidos)
+        {
+            Console.WriteLine($"Bienvenido {nombre} {apellidos}, a la programación modular");
+        }
+
+        //Funciones sin parámetros
+        static int EdadAñoNacimiento()
+        {
+            int añoNacimiento = 1999;
+            int edad = añoActual - añoNacimiento;
+            return edad;
+        }
+
+        //Funciones con parámetros
+        static int EdadAñoNacimiento(int añoNacimiento)
+        {
+            return añoActual - añoNacimiento;
+        }
+
+    }
+}
